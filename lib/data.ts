16d0faa6726ce71ea034cb7a4a95e50cb60e5eae
Neeraj Bhardwaj <nbbhdj143@gmail.com>
2024-01-09 01:42:04 -0500

    first-commit

---
+++ lib/data.ts
@@ -0,0 +1,100 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import corpcommentImg from "@/public/corpcomment.png";
+import rmtdevImg from "@/public/rmtdev.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "B.tech in Computer Science",
+    location: "Bangalore, India",
+    description:
+      "I graduated after 4 years of studying. I immediately found a job as a Full-Stack Developer.",
+    icon: React.createElement(LuGraduationCap),
+    date: "Aug 2018 to Aug 2022",
+  },
+  {
+    title: "Full-Stack Developer",
+    location: "Secpod, Bangalore, India",
+    description:
+      "I worked as a front-end developer for 6 months in Secpod company.",
+    icon: React.createElement(CgWorkAlt),
+    date: "Jan 2022 to Jul 2022",
+  },
+  {
+    title: "Freelancer",
+    // location: "Houston, TX",
+    description:
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, MongoDB. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: "Jul 2022 - present",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "CorpComment",
+    description:
+      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+    imageUrl: corpcommentImg,
+  },
+  {
+    title: "rmtDev",
+    description:
+      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    imageUrl: rmtdevImg,
+  },
+  {
+    title: "Word Analytics",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    imageUrl: wordanalyticsImg,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "MongoDB",
+  "Express", 
+  "Framer Motion",
+] as const;
